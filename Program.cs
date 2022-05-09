d316eec43129c4db4cb99036e8b08eca48da1ba9
Jose Mauro <z.mauro@live.com>
2022-05-08 23:12:22 -0300

    feat: write pokemon in output file in json format

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Net.Http;
-using System.Text.Json;
+using Func = System.Text.Json;
+
 using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 /*
 1- Consumir uma lista de pokemons
     salvar: Name, Types, Abilities, Weight, Height, Sprites
@@ -16,31 +20,52 @@ namespace PokemonAPIClient
 
         static async Task Main() {
             Console.WriteLine("\n   Welcome to PokeAPI!!!!!\n\n────────▄███████████▄────────|\n─────▄███▓▓▓▓▓▓▓▓▓▓▓███▄─────\n────███▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓███────\n───██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██───\n──██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██──\n─██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██─\n██▓▓▓▓▓▓▓▓▓███████▓▓▓▓▓▓▓▓▓██\n██▓▓▓▓▓▓▓▓██░░░░░██▓▓▓▓▓▓▓▓██\n██▓▓▓▓▓▓▓██░░███░░██▓▓▓▓▓▓▓██\n███████████░░███░░███████████\n██░░░░░░░██░░███░░██░░░░░░░██\n██░░░░░░░░██░░░░░██░░░░░░░░██\n██░░░░░░░░░███████░░░░░░░░░██\n─██░░░░░░░░░░░░░░░░░░░░░░░██─\n──██░░░░░░░░░░░░░░░░░░░░░██──\n───██░░░░░░░░░░░░░░░░░░░██───\n────███░░░░░░░░░░░░░░░███────\n─────▀███░░░░░░░░░░░███▀─────\n────────▀███████████▀────────");
-            // string[] pokemonList = { "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Pidgeotto", "Rattata", "Spearow", "Fearow", "Arbok", "Pikachu", "Sandshrew"};
-            string[] pokemonList = {"Ditto"};
+            string[] pokemonList = { "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Pidgeotto", "Rattata", "Spearow", "Fearow", "Arbok", "Pikachu", "Sandshrew"};
+            using StreamWriter file = new("PokemonsInfoList.txt");
+
+            file.Write('[');
             for (int i=0; i< pokemonList.Length; i++){
-                Console.WriteLine("Retrieving {0} from PokeAPI..", pokemonList[i]);
-                await GetPokemon(pokemonList[i]);
+                if (i>0) {file.Write(",\n");}
+                var pokemonInfo = "";
+                pokemonInfo = await GetPokemonInfo(pokemonList[i]);
+                file.Write(pokemonInfo);
             }
+            file.Write(']');
+            Console.WriteLine("Done!");
+
             
         }
 
-        private static async Task GetPokemon(string pokemonName){
+        private static async Task<string> GetPokemonInfo(string pokemonName){
             Console.WriteLine($"Requesting {pokemonName} from PokeAPI (https://pokeapi.co/api/v2/pokemon/{pokemonName.ToLower()})");
-            var streamTask = client.GetStreamAsync($"https://pokeapi.co/api/v2/pokemon/{pokemonName.ToLower()}");
-            // var pokemonInfo = await stringTask;
-            Console.WriteLine("Pokemon info:");
-            // Console.Write(pokemonInfo);
-            var pokemon = await JsonSerializer.DeserializeAsync<Pokemon>(await streamTask);
-        }
-    }
+            var stringTask = client.GetStringAsync($"https://pokeapi.co/api/v2/pokemon/{pokemonName.ToLower()}");
+            // Get JSON with the information about the pokemon
+            var pokemonInfo = JObject.Parse(await stringTask);
+            Dictionary<string, JToken> pokemonInfoDict = new Dictionary<string, JToken>();
+            pokemonInfoDict.Add("name", pokemonInfo["name"]);
+            pokemonInfoDict.Add("weight", pokemonInfo["weight"]);
+            pokemonInfoDict.Add("height", pokemonInfo["height"]);
+            
+            // Get types of the pokemon
+            List<JToken> pokemonTypes = new List<JToken>();
+            foreach (JToken pokemonType in pokemonInfo["types"]){
+                pokemonTypes.Add(pokemonType["type"]["name"]);    
+            }
+            pokemonInfoDict.Add("type", JToken.FromObject(pokemonTypes));
 
-    public class Pokemon{
-        public string name {get; set;}
-        string[] types;
-        string[] abilities;
-        public float weight {get; set;}
-        public float height {get; set;}
-        string[] sprites;
+            // Get the abilities of the pokemon
+            List<JToken> pokemonAbilities = new List<JToken>();
+            foreach (JToken pokemonAbility in pokemonInfo["abilities"]){
+                pokemonAbilities.Add(pokemonAbility["ability"]["name"]);    
+            }
+            pokemonInfoDict.Add("abilities", JToken.FromObject(pokemonAbilities));
+
+
+            // Get the front_default sprite of the pokemon
+            pokemonInfoDict.Add("sprite", pokemonInfo["sprites"]["front_default"]);
+
+            var pokemonInfoResult = JsonConvert.SerializeObject(pokemonInfoDict, Formatting.Indented);
+            return pokemonInfoResult;
+        }
     }
 }
